8560f76d73e2e1d68ffb7854b47368133fcda9b2
Thierry Reding <thierry.reding@avionic-design.de>
2013-01-31 08:49:19 +0100

    Generate checkerboard pattern
    Instead of generating a uniform red "pattern", generate a checkerboard
    pattern. This is in preparation for testing geometric transformations.
    
    Signed-off-by: Thierry Reding <thierry.reding@avionic-design.de>

--- data/pattern.glsl
+++ data/pattern.glsl
@@ -4,5 +4,19 @@ uniform float line_height;
 
 void main()
 {
-   gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    vec3 color, red = vec3(1.0, 0.0, 0.0), blue = vec3(0.0, 0.0, 1.0);
+    vec2 position, pattern, threshold = vec2(0.5);
+    float frequency = 16.0;
+
+    position = vTexcoord * frequency;
+    position = fract(position);
+
+    pattern = step(position, threshold);
+
+    if (pattern.y > 0.0)
+        color = mix(red, blue, pattern.x);
+    else
+        color = mix(blue, red, pattern.x);
+
+    gl_FragColor = vec4(color, 1.0);
 }
