78109cc7ac6582c6941dade5cd2b92057eac910a
Thierry Reding <thierry.reding@avionic-design.de>
2013-02-05 15:46:35 +0100

    Remove unused external shaders
    The shaders are now integrated with the source files of the pipeline
    shaders, which allows the testbench to be run standalone and outside of
    the source tree.
    
    Signed-off-by: Thierry Reding <thierry.reding@avionic-design.de>

--- data/copy.glsl
+++
@@ -1,9 +0,0 @@
-precision mediump float;
-varying vec2 vTexcoord;
-uniform sampler2D s_tex;
-uniform float line_height;
-
-void main()
-{
-    gl_FragColor = vec4(texture2D(s_tex, vTexcoord).rgb, 1.0);
-}
